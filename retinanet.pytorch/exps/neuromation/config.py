a5f874234b2c22a1c5f6e9c4f1840759650411ba
Vladislav Zavadskyy <hort.hope@gmail.com>
2018-04-01 16:44:12 +0300

    a

--- retinanet.pytorch/exps/neuromation/config.py
+++ retinanet.pytorch/exps/neuromation/config.py
@@ -6,9 +6,9 @@ val_path = '/var/hackaton/object_detection/val/SynteticSet20180323_val/data'
 backbone = 'resnet101'
 classes = ['4601201018007', '4600494000393', '5449000050939', '5449000044709', 'noise_bottle', '4601201001412', '4601201018038', '4600494513893']
 mean, std = (60.34319153, 68.21011999, 54.10308942), (1.,1.,1.)
-scale = 500
+scale = 700
 
-batch_size = 2
+batch_size = 4
 lr = 0.01
 momentum = 0.9
 weight_decay = 1e-4
